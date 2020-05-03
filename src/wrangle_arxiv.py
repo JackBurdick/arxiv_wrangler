41b150185cc73cadd76d1cffe26f46762fbd447b
Jack Burdick <jackbburdick@gmail.com>
2020-05-03 11:57:00 -0700

    get citation

--- src/wrangle_arxiv.py
+++ src/wrangle_arxiv.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 
-import requests
-from bs4 import BeautifulSoup
-
+from citation import get_citation
 from defaults import return_defaults
 
 
@@ -12,52 +10,15 @@ def write_local_pdf(pdf_req, pdf_name=None, local_dir_path=None):
     return write_path
 
 
-def get_citation_url(abs_url):
-
-    # I don't remember how to use bs4, so this likely is not the best way to
-    # approach this
-
-    abs_resp = requests.get(abs_url)
-    content = BeautifulSoup(abs_resp.content, "html.parser")
-
-    # outer
-    outter_class_id = "extra-services"
-    content = content.find_all(attrs={"class": outter_class_id})
-    if len(content) > 1:
-        raise ValueError("content in {outter_class_id} is bigger than expected")
-    else:
-        content = content[0]
-
-    # inner
-    class_id = "dblp"
-    content = content.find_all(attrs={"class": class_id})
-    if len(content) > 1:
-        raise ValueError("content in {outter_class_id} is bigger than expected")
-    else:
-        content = content[0]
-
-    # ref link
-    list_items = "a"
-    content = content.find_all(list_items)
-    href_url = None
-    for item in content:
-        href_str = item["href"]
-        if "bibtex" in href_str:
-            href_url = href_str
-
-    if not href_url:
-        raise ValueError(f"bibtex link not found in {content}")
-
-    return href_url
+def get_title(citation):
+    # title = citation.split("=")
+    title = None
+    return title
 
 
-def get_citation(abs_url):
-    bibtex_url = get_citation_url(abs_url)
-
-    # bibtex_resp = requests.get(bibtex_url)
-    # content = BeautifulSoup(bibtex_resp.content, "html.parser")
-
-    return bibtex_url
+def get_cite_id(citation):
+    citation = None
+    return citation
 
 
 if __name__ == "__main__":
@@ -67,19 +28,17 @@ if __name__ == "__main__":
 
     # TODO: config -- currently hardcoded
     abs_url = "https://arxiv.org/abs/1707.08114"
+
+    # get citation
+    citation = get_citation(abs_url)
+    print("citation obtained")
+
+    pdf_title = get_title(citation)
+    citation_id = get_cite_id(citation)
+
     pdf_url = abs_url.replace("abs", "pdf")
 
-    citation = get_citation(abs_url)
-    print(citation)
-
-    # for a in citation:
-    #     print(a.prettify())
-
-    # print(citation.prettify())
-
     # pdf_req = requests.get(pdf_url)
-
-    # TODO: get citation
 
     # TODO: obtain name from citation
     # pdf_name = "new_paper"
