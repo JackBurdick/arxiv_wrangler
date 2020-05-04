3d32f775cfb3a866918630e9d1d79a0b1b0e13e5
Jack Burdick <jackbburdick@gmail.com>
2020-05-03 20:50:11 -0700

    write to bib and tex
    - include citation in specified bib
    - write article title with citation to specified tex file

--- src/defaults.py
+++ src/defaults.py
@@ -2,4 +2,7 @@ def return_defaults():
     d = {}
     d["drive_folder_name"] = "TCF_papers"
     d["local_directory"] = "."
+    d["bib_path"] = "mybib.bib"
+    d["tex_path"] = "mytex.tex"
+    d["tex_wrap"] = "TD"
     return d
--- src/wrangle_arxiv.py
+++ src/wrangle_arxiv.py
@@ -13,6 +13,26 @@ def write_local_pdf(pdf_req, pdf_name=None, local_dir_path=None):
     return write_path
 
 
+def write_to_bib_file(path, citation):
+    # TODO: make sure citation isn't already present
+    with open(path, "a") as myfile:
+        myfile.write(citation)
+
+
+def write_to_tex_file(tex_path, pdf_title, citation_id, tex_wrap):
+    # NOTE: only run if `write_to_bib_file` completes (which will check if it
+    # is already cited).
+    write_str = f"{pdf_title} \\cite{{{citation_id}}}"
+    if tex_wrap:
+        write_str = f"\\{tex_wrap}{{{write_str}}}"
+
+    # wrap in new lines
+    write_str = f"\n{write_str}\n"
+
+    with open(tex_path, "a") as myfile:
+        myfile.write(write_str)
+
+
 if __name__ == "__main__":
     pass
     # TODO: convert to cli
@@ -38,7 +58,7 @@ if __name__ == "__main__":
     )
     print(f"pdf written locally: {local_pdf_path}")
 
-    # # upload to google drive from local
+    # upload to google drive from local
     upload_file_to_drive_folder(
         defaults["drive_folder_name"], pdf_name, local_pdf_path=local_pdf_path
     )
@@ -46,6 +66,14 @@ if __name__ == "__main__":
 
     # TODO: add to bibfile
     # can select which bibfile/location
+    write_to_bib_file(defaults["bib_path"], citation)
+    print("citation written to bibfile")
+
+    # TODO: create TD citation in .tex file
+    write_to_tex_file(
+        defaults["tex_path"], pdf_title, citation_id, defaults["tex_wrap"]
+    )
+    print("citation written to texfile")
 
     # TODO: send to remarkable
     # maybe: https://github.com/dvandyk/syncrm
