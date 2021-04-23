07f280556c8347f54dbb4ade6c74144aec9c204a
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-04-22 21:06:12 -0700

    Update bash_cmd.sh

--- environment/bash_cmd.sh
+++ environment/bash_cmd.sh
@@ -1,8 +1,10 @@
 aw() {
+    cur_env=$(echo $CONDA_DEFAULT_ENV)
     echo 'activate arxiv'
     conda activate arxiv
     cd '/Users/leakymosfet/dev/github/arxiv_wrangler/src'
     python main.py $1
     cd -
+    conda activate $cur_env
     echo 'done arxiv_wrangle'
 }
